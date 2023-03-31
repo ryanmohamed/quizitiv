23cd425e15d725dc557c03d6f0c28ba7da6c74e7
Ryan <reyaznyc@gmail.com>
2023-03-31 01:14:06 -0400

    landing

--- components/NotLoggedIn/NotLoggedIn.tsx
+++ components/NotLoggedIn/NotLoggedIn.tsx
@@ -4,7 +4,7 @@ import img from '../../public/svgs/lock.svg'
 export default function NotLoggedIn () {
     return (<main className={styles.Main}>
         <Image src={img} alt="lock icon" width={100} height={100}/>
-        <h1> You're not signed in </h1>
+        <h1> You&apos;re not signed in </h1>
         <p> Unfortunately you cannot access this page </p>
     </main>)
 }
